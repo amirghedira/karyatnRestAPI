26582bb60e6618e979ac7c29822591e653b1aed7
amir ghedira <amirghedira06@gmail.com>
2020-04-24 01:31:20 +0100

    V1.1

--- routes/user.js
+++ routes/user.js
@@ -6,7 +6,7 @@ const cloudinary = require('../middleware/cloudinary');
 const CheckAuth = require('../middleware/checkAuth')
 router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({ extended: true }))
-
+router.patch('/userimage', CheckAuth, cloudinary.parser.single('userimage'), Usercontroller.updateUserImage);
 router.get('/', Usercontroller.getUsers);
 router.post('/', cloudinary.parser.array('userimages', 3), Usercontroller.addUser);
 router.get('/bytoken', CheckAuth, Usercontroller.getUserbyToken)
@@ -15,7 +15,6 @@ router.get('/username/:username', Usercontroller.getUserbyUsername)
 router.get('/informations', CheckAuth, Usercontroller.getUserInformations)
 router.post('/login', Usercontroller.UserLogin);
 router.patch('/', CheckAuth, Usercontroller.updateUserInfo);
-router.patch('/userimage', CheckAuth, cloudinary.parser.single('userimage'), Usercontroller.updateUserImage);
 router.delete('/:id', Usercontroller.deleteUser);
 
 
