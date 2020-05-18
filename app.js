09960f0e23f59ba0c237c616604ff1d050c2f341
amir ghedira <amirghedira06@gmail.com>
2020-05-18 02:12:19 +0100

    V1.1

--- app.js
+++ app.js
@@ -11,7 +11,11 @@ mongoose.connect(process.env.MONGO_INFO, {
     useNewUrlParser: true
 })
 
-app.all("/*", function (req, res, next) {
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+
+app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
@@ -19,8 +23,6 @@ app.all("/*", function (req, res, next) {
 });
 
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use('/car', carRoutes);
 app.use('/rent', rentRoutes);
